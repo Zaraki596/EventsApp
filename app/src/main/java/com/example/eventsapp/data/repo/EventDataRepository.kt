141225b325a306949abaceb31d8ac5ac56156a79
zaraki <rohansingh94117@gmail.com>
2020-05-17 20:03:49 +0530

    Updated Network Module and Added New EventDataRepository to gather the file

---
+++ app/src/main/java/com/example/eventsapp/data/repo/EventDataRepository.kt
@@ -0,0 +1,20 @@
+package com.example.eventsapp.data.repo
+
+import com.example.eventsapp.data.api.EventsApiService
+import com.example.eventsapp.data.model.EventResponse
+import com.example.eventsapp.utils.State
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+import retrofit2.Response
+
+class EventDataRepository(private val apiService: EventsApiService) {
+
+    @ExperimentalCoroutinesApi
+    fun getEventsData(): Flow<State<EventResponse>> {
+        return object : NetworkBoundRepository<EventResponse>() {
+            override suspend fun fetchFromRemote(): Response<EventResponse> = apiService.getEvents()
+        }.asFlow().flowOn(Dispatchers.IO)
+    }
+}
--- app/src/main/java/com/example/eventsapp/di/NetworkModule.kt
+++ app/src/main/java/com/example/eventsapp/di/NetworkModule.kt
@@ -1,7 +1,12 @@
 package com.example.eventsapp.di
 
+import android.content.Context
 import com.example.eventsapp.data.api.EventsApiService
+import com.example.eventsapp.data.repo.EventDataRepository
+import com.example.eventsapp.utils.isNetworkAvailable
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import okhttp3.Cache
+import okhttp3.OkHttpClient
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -17,4 +22,28 @@ val networkModule = module {
             .build()
             .create(EventsApiService::class.java)
     }
+
+    single {
+        EventDataRepository(get())
+    }
+}
+
+fun getOkHttpClient(context: Context): OkHttpClient {
+    val cacheSize = (5 * 1024 * 1024).toLong()
+    val myCache = Cache(context.cacheDir, cacheSize)
+
+    return OkHttpClient.Builder()
+        .cache(myCache)
+        .addInterceptor { chain ->
+            var request = chain.request()
+            request = if (isNetworkAvailable(context))
+                request.newBuilder().header("Cache-Control", "public, max-age=" + 5).build()
+            else
+                request.newBuilder().header(
+                    "Cache-Control",
+                    "public, only-if-cached, max-stale=" + 60 * 60 * 24 * 7
+                ).build()
+            chain.proceed(request)
+        }
+        .build()
 }
